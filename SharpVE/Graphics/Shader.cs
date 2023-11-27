ff28dc626ca64f7c3fb441753777805c3a45b457
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2023-11-27 13:24:28 +1100

    Modify shader class for customizability.

--- SharpVE/Graphics/Shader.cs
+++ SharpVE/Graphics/Shader.cs
@@ -7,29 +7,37 @@ namespace SharpVE.Graphics
     {
         public int ID { get; set; }
 
-        public Shader(string vertexShaderFilepath, string fragmentShaderFilepath)
+        public Shader()
         {
             ID = GL.CreateProgram();
 
+            //Link shader program to OpenGL
+            GL.LinkProgram(ID);
+        }
+
+        public void AddVertex(string vertexShaderPath)
+        {
             int vertexShader = GL.CreateShader(ShaderType.VertexShader);
             //Add vertex shader source and compile shader
-            GL.ShaderSource(vertexShader, LoadShaderSource(vertexShaderFilepath));
+            GL.ShaderSource(vertexShader, LoadShaderSource(vertexShaderPath));
             GL.CompileShader(vertexShader);
-
-            //Add fragment shader source and compile shader
-            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
-            GL.ShaderSource(fragmentShader, LoadShaderSource(fragmentShaderFilepath));
-            GL.CompileShader(fragmentShader);
 
             //Attach Shaders
             GL.AttachShader(ID, vertexShader);
+
+            GL.DeleteShader(vertexShader);
+        }
+
+        public void AddFragment(string fragmentShaderPath)
+        {
+            //Add fragment shader source and compile shader
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, LoadShaderSource(fragmentShaderPath));
+            GL.CompileShader(fragmentShader);
+
+            //Attach Shaders
             GL.AttachShader(ID, fragmentShader);
 
-            //Link shader program to OpenGL
-            GL.LinkProgram(ID);
-
-            //Delete shaders
-            GL.DeleteShader(vertexShader);
             GL.DeleteShader(fragmentShader);
         }
 
