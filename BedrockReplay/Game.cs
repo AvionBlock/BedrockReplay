0b3e8e018928bc20c42a6bfaf9b5a81630de1ecb
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-08-25 11:18:33 +1000

    Oh.

--- BedrockReplay/Game.cs
+++ BedrockReplay/Game.cs
@@ -3,6 +3,7 @@ using BedrockReplay.Components;
 using BedrockReplay.ComponentSystems;
 using BedrockReplay.Managers;
 using BedrockReplay.Shaders;
+using SharpVE.Blocks;
 using Silk.NET.Windowing;
 using System.Drawing;
 
@@ -10,6 +11,7 @@ namespace SharpVE
 {
     public class Game
     {
+        public static World<BlockState> World { get; private set; } = new World<BlockState>();
         public static Arch.Core.World ECSWorld = Arch.Core.World.Create();
         public static Group<double> Systems = new Group<double>("systems",
             new CameraSystem(ECSWorld)
@@ -38,7 +40,12 @@ namespace SharpVE
             //ECSWorld.Create(new MeshRendererComponent(), new TransformComponent());
 
             Systems.Initialize();
-        }
+            World.ChunkManager.CreateChunk(new Data.BlockPosition(0, 0, 0), new BlockState(new Block("minecraft:grass")));
+            var chunk = World.ChunkManager.GetChunk(new Data.BlockPosition(0, 0, 0));
+            var block = chunk.GetBlockState(0, 0, 0);
+            Console.WriteLine(chunk);
+            Console.WriteLine(block);
+;       }
 
         private void WindowUpdate(WindowInstance window, double delta)
         {
