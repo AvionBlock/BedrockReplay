9039d166d14effeaad7f6ebe4538f2570aed1d4b
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-08-30 20:32:10 +1000

    Some Updates.

--- BedrockReplay/Game.cs
+++ BedrockReplay/Game.cs
@@ -10,12 +10,13 @@ using Silk.NET.Input;
 using Silk.NET.Windowing;
 using System.Drawing;
 using SharpVE;
+using Silk.NET.Maths;
 
 namespace BedrockReplay
 {
     public class Game
     {
-        public static readonly World<BlockState> World  = new World<BlockState>();
+        public static readonly World<BlockState> World = new World<BlockState>();
         public static readonly World ECSWorld = Arch.Core.World.Create();
 
         public static readonly Group<double> Systems = new Group<double>("systems",
@@ -48,13 +49,21 @@ namespace BedrockReplay
             mouse.Cursor.CursorMode = CursorMode.Raw;
 
             ECSWorld.Create(new CameraComponent(projShader, window), new TransformComponent(0, 0, 0), new FPSController(keyboard, mouse));
-            ECSWorld.Create(new ChunkMeshComponent() { Mesh = window.Engine.CreateMesh(plane.vertices, plane.Indices) }, new TransformComponent(0,0,-1) { Rotation = Silk.NET.Maths.Quaternion<float>.CreateFromAxisAngle(new Silk.NET.Maths.Vector3D<float>(0, 0, 1), MathHelper.DegreesToRadians(90)) });
+            ECSWorld.Create(new ChunkMeshComponent() { Mesh = window.Engine.CreateMesh(plane.vertices, plane.Indices) }, new TransformComponent(0, 0, -1) { Rotation = Silk.NET.Maths.Quaternion<float>.CreateFromAxisAngle(new Silk.NET.Maths.Vector3D<float>(0, 0, 1), MathHelper.DegreesToRadians(90)) });
 
             Systems.Initialize();
         }
 
         private static void WindowUpdate(WindowInstance window, double delta)
         {
+            var query = new QueryDescription()
+                .WithAll<ChunkMeshComponent, TransformComponent>();
+
+            ECSWorld.Query(in query, (ref ChunkMeshComponent chunkMesh, ref TransformComponent transformComponent) =>
+            {
+                transformComponent.Rotation *= Quaternion<float>.CreateFromAxisAngle(Vector3D<float>.UnitX, MathHelper.DegreesToRadians(1));
+            });
+
             Systems.BeforeUpdate(delta);
             Systems.Update(delta);
         }
